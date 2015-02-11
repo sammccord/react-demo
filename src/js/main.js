0898a69d879d24c2fe76c3a7c292b7e67ad326ff
Sam McCord <samuel.e.mccord@gmail.com>
2015-02-10 23:24:15 -0500

    a little more prep

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,4 @@ var _comments = [
     {author:'President Merkin Muffley',text:'Gentlemen, you can\'t fight in here! This is the War Room.'},
     {author:'Dr. Strangelove', text:'Of course, the whole point of a Doomsday Machine is lost, if you *keep* it a *secret*! Why didn\'t you tell the world, EH?'}
 ];
+
